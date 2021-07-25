5d7d43e04a7593423a1c70ff4a2c2f0118ae4b5d
John George <Sci.JohnGeorge.2017@alexu.edu.eg>
2021-07-25 16:56:50 +0200

    Add tests for user and admin user.

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'before publication' do
+    include_examples "authenticatable", 'admin_user'
+
+    it "validates creation using email and password only" do
+      admin_user = AdminUser.new :email => Faker::Internet.email, :password => "password"
+      expect(admin_user).to be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'before publication' do
+    include_examples "authenticatable", 'user'
+
+    it "validates creation using email and password only" do
+      user = User.new :email => Faker::Internet.email, :password => "password"
+      expect(user).to be_valid
+    end
+  end
 end
