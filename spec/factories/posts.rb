195a05fd87767ff9d39e85022ee7e5daf320bdda
John George <Sci.JohnGeorge.2017@alexu.edu.eg>
2021-07-27 14:43:42 +0200

    Add tests to post

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :post do
-    title { "MyString" }
-    body { "MyText" }
-    category { nil }
+    title { Faker::Lorem.characters[0, 30] }
+    body { Faker::Lorem.paragraph(sentence_count: 3) }
+    category
+    association :creator, factory: :user
   end
 end
